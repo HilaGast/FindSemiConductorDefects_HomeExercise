d8457e4bf008d38504df15dfed3aa22cf660cf3c
Hila Gast <hilagast@mail.tau.ac.il>
2023-10-05 01:35:19 +0300

    renaming

--- detect_differences.py
+++ detect_differences.py
@@ -1,84 +1,102 @@
-from typing import Iterable
-import cv2 as cv
-import cv2
+from typing import Tuple, Sequence
 import numpy as np
 import matplotlib.pyplot as plt
 import skimage
+from cv2 import (
+    DMatch,
+    KeyPoint,
+    imread,
+    IMREAD_GRAYSCALE,
+    NORM_HAMMING,
+    BFMatcher,
+    estimateAffinePartial2D,
+    fastNlMeansDenoising,
+    connectedComponentsWithStats,
+    warpAffine,
+)
+import cv2
 from scipy.ndimage import gaussian_filter
 from skimage.morphology import dilation, disk
 from sklearn.cluster import KMeans
 import argparse
 
 
-def find_defects(reference: str, inspected: str, plot_flag: bool = True) -> None:
-    """Find defects in an inspected image compared to a reference image
+def find_defects(reference_path: str, inspected_path: str, should_plot: bool = True) -> None:
+    """
+    Find defects in an inspected image compared to a reference image
     Input:
         reference: path to reference image
         inspected: path to inspected image
     Output:
-        None"""
+        None
+    """
 
-    # Read the images:
-    ref_img = read_image(reference)
-    insp_img = read_image(inspected)
+    # Read the images
+    ref_img = read_image(reference_path)
+    insp_img = read_image(inspected_path)
 
-    # Pre-processing:
-    denoised_matched, denoised_translated = preprocess(ref_img, insp_img)
+    # Pre-processing
+    preprocessed_insp, preprocessed_ref = preprocess(insp_img, ref_img)
 
-    # Create 1st mask using kmeans and the differences in classes between the two images:
-    kmeans_mask = create_mask_kmeans(denoised_matched, denoised_translated, min_size=30)
-    if plot_flag:
+    # Create 1st mask using the differences between kmeans clusters of the two images
+    kmeans_mask = create_kmeans_mask(preprocessed_insp, preprocessed_ref, object_min_size=30)
+    if should_plot:
         plot_results(
-            ref_img,
-            insp_img,
-            kmeans_mask,
-            labels=["reference", "inspected", "Kmeans mask"],
-            title="Kmeans mask result",
+            ref_img, insp_img, kmeans_mask, labels=("reference", "inspected", "Kmeans mask"), title="Kmeans mask result"
         )
 
-    # Create 2nd mask using the differences contours between the two images:
-    contour_mask = create_mask_contours(
-        denoised_matched,
-        denoised_translated,
+    # Create 2nd mask using the contours difference between the two images
+    contour_mask = create_contours_mask(
+        preprocessed_insp,
+        preprocessed_ref,
         sigma_contour=1,
         sigma_gaussian=2,
         kernel_size=3,
-        min_size=100,
+        object_min_size=100,
     )
 
-    if plot_flag:
+    if should_plot:
         plot_results(
             ref_img,
             insp_img,
             contour_mask,
-            labels=["reference", "inspected", "contour mask"],
+            labels=("reference", "inspected", "contour mask"),
             title="Contour mask result",
         )
 
     # Combine masks, keeping only the pixels that are different in both masks:
     mask = kmeans_mask * contour_mask
 
-    if plot_flag:
+    if should_plot:
         plot_results(
             ref_img,
             insp_img,
             mask,
-            labels=["reference", "inspected", "mask"],
+            labels=("reference", "inspected", "mask"),
             title="Masks",
         )
 
 
-def preprocess(ref_img: np.ndarray, insp_img: np.ndarray) -> tuple:
-    # Image normalization for intensity matching - Histogram matching:
-    matched_img = histogram_matching(ref_img, insp_img)
+def read_image(path: str) -> np.ndarray:
+    # Read the image and convert to grayscale:
+    img = imread(path, IMREAD_GRAYSCALE)
+    return img
 
-    # Image registration - 3 steps:
+
+def preprocess(
+    insp_img: np.ndarray,
+    ref_img: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray]:
+    # Match the intensity of the inspected image using Histogram matching:
+    hist_matched_insp_img = match_histogram(ref_img, insp_img)
+
+    # Image registration - we will move the reference image to match the inspected image:
     # Step 1: Feature matching
     moving_keypoints, target_keypoints, top_matches = get_matching_features_orb(
         ref_img,
-        matched_img,
+        hist_matched_insp_img,
         nfeatures=1000,
-        threshold=0.5,
+        top_matches_to_use=0.5,  # use only the top 50% of the features
     )
 
     # Step 2: Calculate the translation matrix
@@ -88,24 +106,18 @@ def preprocess(ref_img: np.ndarray, insp_img: np.ndarray) -> tuple:
         top_matches,
     )
 
-    # Step 3: Register the image
-    translated_img = register_image(ref_img, translation_matrix)
-    matched_img[translated_img == 0] = 0
+    # Step 3: Register the reference image
+    translated_ref_img = register_image(ref_img, translation_matrix)
+    hist_matched_insp_img[translated_ref_img == 0] = 0
 
-    # Image denoising:
-    denoised_matched = denoise_image(matched_img, h=7, templateWindowSize=7, searchWindowSize=21)
-    denoised_translated = denoise_image(translated_img, h=7, templateWindowSize=7, searchWindowSize=21)
+    # Image denoise:
+    denoised_insp_img = denoise_image(hist_matched_insp_img, h=7, template_window_size=7, search_window_size=21)
+    denoised_ref_img = denoise_image(translated_ref_img, h=7, template_window_size=7, search_window_size=21)
 
-    return denoised_matched, denoised_translated
-
-
-def read_image(path: str) -> np.ndarray:
-    # Read the image and convert to grayscale:
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    return img
+    return denoised_insp_img, denoised_ref_img
 
 
-def histogram_matching(reference: np.ndarray, moving: np.ndarray) -> np.ndarray:
+def match_histogram(reference: np.ndarray, moving: np.ndarray) -> np.ndarray:
     # Perform histogram matching between two images:
     matched = skimage.exposure.match_histograms(moving, reference)
     matched = matched.astype("uint8")
@@ -116,43 +128,43 @@ def get_matching_features_orb(
     moving: np.ndarray,
     target: np.ndarray,
     nfeatures: int,
-    threshold: float = 0.75,
-) -> tuple:
+    top_matches_to_use: float = 0.5,
+) -> tuple[Sequence[KeyPoint], Sequence[KeyPoint], list[DMatch]]:
     """get matching features between two images using ORB
     Input:
         target: reference image for registration
         moving: moving image to register
     Output:
-        kp1: keypoints of the moving image
-        kp2: keypoints of the target image
+        kp1: key points of the moving image
+        kp2: key points of the target image
         matches: matching features
     """
     # Initiate ORB detector:
     orb = cv2.ORB_create(nfeatures)
 
-    # Find the keypoints and descriptors with ORB:
-    moving_keypoints, des1 = orb.detectAndCompute(moving, None)
-    target_keypoints, des2 = orb.detectAndCompute(target, None)
+    # Find the key points and descriptors with ORB:
+    moving_keypoints, moving_descriptors = orb.detectAndCompute(moving, mask=None)
+    target_keypoints, target_descriptors = orb.detectAndCompute(target, mask=None)
 
     # Create BFMatcher object:
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    bf = BFMatcher(NORM_HAMMING, crossCheck=True)
 
     # Match descriptors:
-    matches = bf.match(des1, des2)
+    matches = bf.match(moving_descriptors, target_descriptors)
 
-    # Sort them in the order of their distance:
+    # Sort descriptors by their distance:
     matches = sorted(matches, key=lambda x: x.distance)
 
-    # Take the best top_matches% of the matches:
-    top_matches = matches[: int(len(matches) * threshold)]
+    # Take the best matches % of the matches:
+    top_matches = matches[: int(len(matches) * top_matches_to_use)]
 
     return moving_keypoints, target_keypoints, top_matches
 
 
 def calculate_translation_matrix(
-    moving_keypoints: list,
-    target_keypoints: list,
-    matches: list,
+    moving_keypoints: Sequence[KeyPoint],
+    target_keypoints: Sequence[KeyPoint],
+    matches: list[DMatch],
 ) -> np.ndarray:
     """calculate the translation matrix between two images
     Input:
@@ -162,16 +174,16 @@ def calculate_translation_matrix(
     Output:
         translation_matrix: translation matrix between the two images
     """
-    # Define empty matrices of shape no_of_matches * 2.
-    p1 = np.zeros((len(matches), 2))
-    p2 = np.zeros((len(matches), 2))
+    matches_count = len(matches)
+    p1 = np.zeros((matches_count, 2))
+    p2 = np.zeros((matches_count, 2))
 
-    for i in range(len(matches)):
+    for i in range(matches_count):
         p1[i, :] = moving_keypoints[matches[i].queryIdx].pt
         p2[i, :] = target_keypoints[matches[i].trainIdx].pt
 
-    # Find the translation matrix:
-    translation_matrix, inliers = cv2.estimateAffinePartial2D(p1, p2)
+    # Find the translation matrix, ignore inliers
+    translation_matrix, _ = estimateAffinePartial2D(p1, p2)
 
     return translation_matrix
 
@@ -179,7 +191,7 @@ def calculate_translation_matrix(
 def register_image(img: np.ndarray, translation_matrix: np.ndarray) -> np.ndarray:
     # Register an image using a translation matrix:
     x, y = img.shape
-    registered = cv2.warpAffine(img, translation_matrix, (y, x))
+    registered = warpAffine(img, translation_matrix, (y, x))
 
     return registered
 
@@ -187,27 +199,29 @@ def register_image(img: np.ndarray, translation_matrix: np.ndarray) -> np.ndarra
 def denoise_image(
     img: np.ndarray,
     h: int = 7,
-    templateWindowSize: int = 7,
-    searchWindowSize: int = 21,
+    template_window_size: int = 7,
+    search_window_size: int = 21,
 ) -> np.ndarray:
     """denoise an image using non-local means denoising
     Input:
         img: image
         h: level of denoising
-        templateWindowSize: template window size for the denoising
-        searchWindowSize: search window size for the denoising
+        template_window_size: template window size for the denoising
+        search_window_size: search window size for the denoising
     Output:
         denoised_img: denoised image
     """
-    denoised_img = cv.fastNlMeansDenoising(img, None, h, templateWindowSize, searchWindowSize)
+    denoised_img = fastNlMeansDenoising(
+        img, None, h, templateWindowSize=template_window_size, searchWindowSize=search_window_size
+    )
 
     return denoised_img
 
 
-def create_mask_kmeans(
+def create_kmeans_mask(
     img1: np.ndarray,
     img2: np.ndarray,
-    min_size: int = 30,
+    object_min_size: int = 30,
 ) -> np.ndarray:
     """Create a mask using kmeans for three instensity groups and the differences in classes between the two images
     Input:
@@ -218,31 +232,31 @@ def create_mask_kmeans(
         mask: mask of the differences between the two images
     """
 
-    # Create kmeans mask where k=3 for bot images:
-    mask_img1 = sorted_labels_kmeans(img1, n_clusters=3)
-    mask_img2 = sorted_labels_kmeans(img2, n_clusters=3)
+    # Create kmeans mask where k=3 for both images:
+    mask_img1 = get_sorted_labels_kmeans(img1, n_clusters=3)
+    mask_img2 = get_sorted_labels_kmeans(img2, n_clusters=3)
 
     # Find the differences between the two masks in each class
-    mask0 = mask_img1[:, :, 0] != mask_img2[:, :, 0]
-    mask1 = mask_img1[:, :, 1] != mask_img2[:, :, 1]
-    mask2 = mask_img1[:, :, 2] != mask_img2[:, :, 2]
+    cluster_0_mask = mask_img1[:, :, 0] != mask_img2[:, :, 0]
+    cluster_1_mask = mask_img1[:, :, 1] != mask_img2[:, :, 1]
+    cluster_2_mask = mask_img1[:, :, 2] != mask_img2[:, :, 2]
 
     # Add masks together:
-    mask = mask0.astype("uint8") + mask1.astype("uint8") + mask2.astype("uint8")
+    combined_mask = cluster_0_mask.astype("uint8") + cluster_1_mask.astype("uint8") + cluster_2_mask.astype("uint8")
 
     # Remove small objects:
-    mask = find_and_remove_small_objects(mask, threshold=min_size)
+    combined_mask = find_and_remove_small_objects(combined_mask, threshold=object_min_size)
 
     # Binarize for final mask:
-    mask[mask > 0] = 1
+    combined_mask[combined_mask > 0] = 1
 
-    return mask
+    return combined_mask
 
 
-def sorted_labels_kmeans(
+def get_sorted_labels_kmeans(
     img: np.ndarray,
     n_clusters: int = 3,
-    init: list = [[10], [127], [220]],
+    init: tuple[list[int], ...] = ([10], [127], [220]),
 ) -> np.ndarray:
     """Create a mask using kmeans for n instensity groups:
     Input:
@@ -266,13 +280,13 @@ def sorted_labels_kmeans(
     return mask
 
 
-def create_mask_contours(
+def create_contours_mask(
     img1: np.ndarray,
     img2: np.ndarray,
     sigma_contour: int = 1,
     sigma_gaussian: int = 2,
     kernel_size: int = 3,
-    min_size: int = 30,
+    object_min_size: int = 30,
 ) -> np.ndarray:
     """Create a mask using the differences contours between the two images
     Input:
@@ -309,18 +323,18 @@ def create_mask_contours(
     ssim = 1 - ssim
 
     # Threshold the ssim image:
-    th = skimage.filters.threshold_otsu(ssim)
-    ssim[ssim < th] = 0
-    mask = ssim * 255
-    mask = mask.astype("uint8")
+    threshold = skimage.filters.threshold_otsu(ssim)
+    ssim[ssim < threshold] = 0
+    contours_mask = ssim * 255
+    contours_mask = contours_mask.astype("uint8")
 
     # Remove small objects:
-    mask = find_and_remove_small_objects(mask, threshold=min_size)
+    contours_mask = find_and_remove_small_objects(contours_mask, threshold=object_min_size)
 
     # Binarize for final mask:
-    mask[mask > 0] = 1
+    contours_mask[contours_mask > 0] = 1
 
-    return mask
+    return contours_mask
 
 
 def detect_contours(
@@ -349,7 +363,7 @@ def detect_contours(
 
 
 def ssim_images(img1: np.ndarray, img2: np.ndarray) -> np.ndarray:
-    # Calculate the structural similarity index between two images:
+    # Calculate the structural similarity index between two images
     score, diff_img = skimage.metrics.structural_similarity(img1, img2, full=True)
 
     return diff_img
@@ -364,29 +378,29 @@ def find_and_remove_small_objects(img: np.ndarray, threshold: int = 20) -> np.nd
     Output:
         img: np.ndarray of the image with the small objects removed
     """
-    # find all your connected components (white blobs in the image):
-    nb_blobs, img_with_separated_blobs, stats, _ = cv2.connectedComponentsWithStats(img)
+    # find all connected components (white blobs in the image):
+    blobs_count, img_with_separated_blobs, stats, _ = connectedComponentsWithStats(img)
 
-    # Removing background from the list:
+    # Remove background from the list:
     sizes = stats[:, -1]
     sizes = sizes[1:]
-    nb_blobs -= 1
+    blobs_count -= 1
 
     # For every component in the image, keep it only if it's above threshold:
-    img_cleaned = np.zeros_like(img_with_separated_blobs)
-    for blob in range(nb_blobs):
+    cleaned_img = np.zeros_like(img_with_separated_blobs)
+    for blob in range(blobs_count):
         if sizes[blob] >= threshold:
             # see description of im_with_separated_blobs above
-            img_cleaned[img_with_separated_blobs == blob + 1] = 255
+            cleaned_img[img_with_separated_blobs == blob + 1] = 255
 
-    return img_cleaned
+    return cleaned_img
 
 
 def plot_results(
     img1: np.ndarray,
     img2: np.ndarray,
     img3: np.ndarray,
-    labels: Iterable[str] = ("img1", "img2", "img3"),
+    labels: Tuple[str, ...] = ("img1", "img2", "img3"),
     title: str = "",
 ) -> None:
     # Plot the results comparing 3 images
@@ -398,7 +412,7 @@ def plot_results(
     ax[2].imshow(img3, cmap="gray")
     ax[2].set_xlabel(labels[2], fontsize=16)
     fig.suptitle(title, fontsize=24)
-    plt.show()  # type: ignore
+    plt.show()
 
 
 def main() -> None:
